0b5973dde9920bacf2f752d0348b3fad31f08c3f
J0seFY <joseignaciofuentesyanez@gmail.com>
2025-05-05 16:21:02 -0400

    implementacion AuthService para registro y login de usuarios y creacion de docker compose

---
+++ AuthService/src/main/java/com/authservice/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.authservice.repository;
+
+import com.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+
+    Optional<User> findByRut(String rut);
+
+    Optional<User> findByRutAndPassword(String rut, String password);
+}
