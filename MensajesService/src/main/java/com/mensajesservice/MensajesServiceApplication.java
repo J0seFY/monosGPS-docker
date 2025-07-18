c14a859742656435f298a8a29b74a49414cffed3
J0seFY <joseignaciofuentesyanez@gmail.com>
2025-07-18 17:27:59 -0400

    Mensajes

---
+++ MensajesService/src/main/java/com/mensajesservice/MensajesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mensajesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensajesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MensajesServiceApplication.class, args);
+    }
+
+}
---
+++ MensajesService/src/test/java/com/mensajesservice/MensajesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mensajesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensajesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
