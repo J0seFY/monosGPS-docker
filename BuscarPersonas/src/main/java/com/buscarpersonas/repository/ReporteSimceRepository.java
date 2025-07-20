11d0550ba50cdd81a4cfe3146b489dd353dac989
Julisa Figueroa <julisa.figueroa2101@alumnos.ubiobio.cl>
2025-07-20 17:32:33 -0400

    intento mil 2

--- BuscarPersonas/src/main/java/com/buscarpersonas/repository/ReporteSimceRepository.java
+++ BuscarPersonas/src/main/java/com/buscarpersonas/repository/ReporteSimceRepository.java
@@ -1,13 +1,15 @@
 package com.buscarpersonas.repository;
 
-import com.buscarpersonas.dto.ReporteSimceDTO;
+import com.buscarpersonas.Entity.ResultadoPrueba;
+import com.buscarpersonas.dto.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ReporteSimceRepository extends JpaRepository<ReporteSimceDTO, Long> {
+public interface ReporteSimceRepository extends JpaRepository<ResultadoPrueba, Long> {
+
     @Query("SELECT new com.buscarpersonas.dto.ReporteSimceDTO(e.rut, e.nombre, e.apellido, est.nombre, r.puntaje, r.fecha) " +
            "FROM ResultadoPrueba r " +
            "JOIN Estudiante e ON r.estudianteRut = e.rut " +
