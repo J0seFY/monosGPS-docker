d08ec345ce52b7f63c402196fb27f59cc8594d72
christian <christian.diaz2101@alumnos.ubiobio.cl>
2025-07-20 11:59:29 -0400

    Reporte matriculas de un colegio

---
+++ BuscarPersonas/src/main/java/com/buscarpersonas/Controller/ReporteMatriculaColegioController.java
@@ -0,0 +1,29 @@
+// ReporteMatriculaController.java
+package com.buscarpersonas.Controller;
+
+import com.buscarpersonas.service.ReporteMatriculaColegioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reportes")
+public class ReporteMatriculaColegioController {
+
+    @Autowired
+    private ReporteMatriculaColegioService reporteService;
+
+    @GetMapping("/matricula/{idEstablecimiento}")
+    public ResponseEntity<InputStreamResource> descargarReporteMatricula(@PathVariable Integer idEstablecimiento) {
+        var pdfStream = reporteService.generarPdfPorEstablecimiento(idEstablecimiento);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=reporte_matricula.pdf");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(pdfStream));
+    }
+}
