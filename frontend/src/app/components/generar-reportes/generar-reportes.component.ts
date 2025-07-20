a6a831bd5fe08501a89967ce40793571b3c8b45e
Julisa Figueroa <julisa.figueroa2101@alumnos.ubiobio.cl>
2025-07-20 11:22:30 -0400

    reales reportes

--- frontend/src/app/components/generar-reportes/generar-reportes.component.ts
+++ frontend/src/app/components/generar-reportes/generar-reportes.component.ts
@@ -17,16 +17,13 @@ export class GenerarReportesComponent {
     'Listado de alumnos retirados',
     'Listado de posibles repitencias por establecimiento, curso y asignaturas',
     'Listado de accidentes escolares a nivel comunal',
-    'Enviar mensajes a los apoderados a nivel comunal',
     'Reportes de resultados comunal: SIMCE',
     'Reportes de resultados comunal: PAES ',
     'Reportes de resultados comunal: Cobertura Curricular',
     'Informe de asistencia diaria de todos los establecimientos',
     'Informe de rendimiento a nivel comunal y por establecimiento',
     'Reporte de inasistencias a nivel comunal y por establecimiento',
-    'Sistema de mensajería a nivel sostenedor por perfiles',
     'Reporte de funcionarios a nivel comunal y por establecimiento',
-    'Búsqueda de personas: alumnos, apoderados, profesores, administrativos',
   ];
 
   constructor(private http: HttpClient) {}
