c8c5c814a89b6b286cb489ec4b2325383a450598
christian <christian.diaz2101@alumnos.ubiobio.cl>
2025-07-20 12:59:50 -0400

    segundo cambio de rutas

--- BuscarPersonas/src/main/java/com/buscarpersonas/Controller/ReporteFuncionariosController.java
+++ BuscarPersonas/src/main/java/com/buscarpersonas/Controller/ReporteFuncionariosController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/reportesFuncionarios")
+@RequestMapping("/api/reportesFuncionarios")
 public class ReporteFuncionariosController {
 
     @Autowired
